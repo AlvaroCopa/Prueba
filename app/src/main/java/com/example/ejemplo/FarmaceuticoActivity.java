3c7f0a51f9dbb8368f569bf1b9714361c1979198
jhongary <garymarin6@gmail.com>
2021-01-25 23:14:52 -0400

    activity recepcion reseta y Farmaceutico

---
+++ app/src/main/java/com/example/ejemplo/FarmaceuticoActivity.java
@@ -0,0 +1,16 @@
+package com.example.ejemplo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class FarmaceuticoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmaceutico);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/ejemplo/RecepcionRecetaActivity.java
@@ -0,0 +1,15 @@
+package com.example.ejemplo;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecepcionRecetaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recepcion_receta);
+
+    }
+}
